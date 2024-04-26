90f9d7fdc3b325406c7dd22edcbd7f9eab94b5b3
Martin Andreev <martin_i_a@abv.bg>
2024-04-26 17:30:16 +0300

    187 useReducer introduction
    Pyrva lekciq za useReducer

--- 10-react-quiz/src/DateCounter.js
+++ 10-react-quiz/src/DateCounter.js
@@ -1,25 +1,45 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  console.log(state, action); // 0 1
+  if (action.type === "inc") return state + 1;
+  if (action.type === "dec") return state - 1;
+  if (action.type === "setCount") return action.payload;
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+
+  // When does the reducer function get called? That's where the dispatch here actually comes into play. So we
+  // see that the useReducer hook does return the current state, so just like the used state hook. But then
+  // instead of also returning a state updating function, the useReducer hook returns as a second thing, this
+  // dispatch function. And so this dispatch function can also be used to update the state. It just works in a
+  // slightly different way.
+  const [count, dispatch] = useReducer(reducer, 0);
+
   const [step, setStep] = useState(1);
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
+  // V konkretno tezi sluchai payload-a e izlishen, mozhem napravo gore da napishem syotvetno + 1 i -1.
+  // V defineCount obache veche si e nuzhno
   const dec = function () {
+    dispatch({ type: "dec" });
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    // setCount((count) => count - step);
   };
 
   const inc = function () {
+    dispatch({ type: "dec" });
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
+    // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
@@ -27,7 +47,7 @@ function DateCounter() {
   };
 
   const reset = function () {
-    setCount(0);
+    // setCount(0);
     setStep(1);
   };
 
