1e2f34d14d531e81053e2d2c396dccdf98b6c353
Martin Andreev <martin_i_a@abv.bg>
2024-04-27 17:00:36 +0300

    207 linking between routes with <Link /> and <Navlink />
    TOVA E S Link (t.e. vse edno pyrva chast)

--- 11-worldwise/src/pages/Homepage.jsx
+++ 11-worldwise/src/pages/Homepage.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function Homepage() {
-    return <div>WorldWise</div>;
+    return (
+        <div>
+            <PageNav />
+            <h1>WorldWise</h1>
+            {/* <a href="/pricing">Pricing</a> taka stranicata shte se prezarezhda, a ne iskame tova
+            Tova mozhe da se proveri v Network -> All
+            Zatova izpolzvame Link: */}
+
+            <Link to="/pricing">Pricing</Link>
+        </div>
+    );
 }
 
 export default Homepage;
