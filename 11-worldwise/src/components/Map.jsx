e1c817614f558a8af68ac700ba1e7f454ef926f0
Martin Andreev <martin_i_a@abv.bg>
2024-04-28 23:11:29 +0300

    231 Displaying City Markers on Map
    TRIEM FALSHIVO SYZDADENIQ grad, za da ne stane obyrkvane.
    
    This is the power of having global state. Whenever you need a piece of state, you just grab it, and there is it. You don't have to pass any props.
    
    Ako shte implementiram nqkoga map, moga da izpolzvam stilovete ot Map.module.css (vizh vyv videoto koi tochno, sled 4-tata minuta).

--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -3,9 +3,14 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 import styles from "./Map.module.css";
 import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 /* NavLink e deklarativen metod za prenasochvane, a useNavigate - imperativen!! */
 function Map() {
   const navigate = useNavigate();
+  /* This is the power of having global state. Whenever you need a piece of state, you just grab it, and there
+  is it. You don't have to pass any props. */
+  const { cities } = useCities();
+
   const [mapPosition, setMapPosition] = useState([40, 0]);
   // tezi chisla v useState-a za lat i lng
 
@@ -26,11 +31,16 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
