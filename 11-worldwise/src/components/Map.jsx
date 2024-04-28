6ed4c63f3f6a772207e4adb6cd0bbd75927b99c6
Martin Andreev <martin_i_a@abv.bg>
2024-04-28 22:17:13 +0300

    230 Including a Map With the Leaflet Library
    implementirame map s pomoshtta na third party package -> npm i react-leaflet leaflet (the biggest open source library to implement maps).
    
    Kato implementirame biblioteka, e dobra ideq da otvorim dokumentaciqta (v tozi sluchay pishem react leaflet).
    
    Instrukcii nqkyde sled 2-rata minuta na lekciqta.
    
    Pravihme i nqkoi nestandartni neshta, syshto i neshta, svyrzani s CSS-a.
    
    Po-dobre da se gleda videoto, ako shte se pravi podobno neshto.

--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -1,33 +1,39 @@
-import { useNavigate, useSearchParams } from 'react-router-dom';
-import styles from './Map.module.css';
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+import styles from "./Map.module.css";
+import { useState } from "react";
 /* NavLink e deklarativen metod za prenasochvane, a useNavigate - imperativen!! */
 function Map() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  // tezi chisla v useState-a za lat i lng
 
-    const [searchParams, setSearchParams] = useSearchParams();
-    const lat = searchParams.get("lat");
-    const lng = searchParams.get("lng");
-    // i tezi 3 neshta otgore mozhe da gi izpolzvame i v drugi komponenti, primerno v City (prehvyrli se tam).
-    return (
-        <div className={styles.mapContainer} onClick={() => navigate("form")}>
-            {/* i taka kydeto i da cyknem na map stranicata, shte ni zavede vyv form (t.e. shte ni otvori forma) */}
-            <h1>Map</h1>
-            <h1>Position: {lat}, {lng} </h1>
-            {/* i taka v map prozoreca izpisva:
-        Map
-        Position: 50.53586782505711, 14.376933665713324
-        A MOZHEM i da promenim tezi st-ti sys setSearchParams tuk po-dolu eto taka:
- */}
-            <button onClick={() => {
-                setSearchParams({ lat: 23, lng: 50 });
-            }}
-            >
-                Change Pos
-            </button>
-            {/* i taka sega ako cyknem na Change Pos butona - shte go promeni lat-a i lng-a navsqkyde v
-            prilozhenieto, ne samo na edno mqsto */}
-        </div >
-    );
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  // i tezi 3 neshta otgore mozhe da gi izpolzvame i v drugi komponenti, primerno v City (prehvyrli se tam).
+  return (
+    <div className={styles.mapContainer}>
+      {/* i taka kydeto i da cyknem na map stranicata, shte ni zavede vyv form (t.e. shte ni otvori forma) */}
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
 
 export default Map;
