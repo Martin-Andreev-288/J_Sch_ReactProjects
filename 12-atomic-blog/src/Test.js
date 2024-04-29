5fd65c55d3086e49923e3042c31c24d7bab1e400
Martin Andreev <martin_i_a@abv.bg>
2024-04-29 20:21:23 +0300

    245 A Surprising Optimization Trick With children
    EKSPERIMENT kak da optimizirame byrzina s children-i (ne vseki go znae, taka che mozhe bi ne e zadylzhitelno da go zapomnqme do kray).
    
    Za da go vidim, tr da otkomentirame <Test /> v App.js
    
    Po-dobre da se gleda videoto, za da se izqsni

--- 12-atomic-blog/src/Test.js
+++ 12-atomic-blog/src/Test.js
@@ -14,13 +14,38 @@ function SlowComponent() {
   );
 }
 
-export default function Test() {
+function Counter({ children }) {
   const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+
+      {children}
+    </div>
+  );
+}
+
+export default function Test() {
+  // const [count, setCount] = useState(0);
+  // return (
+  //   <div>
+  //     <h1>Slow counter?!?</h1>
+  //     <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+
+  //     {/* taka (v pyrvonachalnata versiq) vseki pyt, kogato kliknem na icrease butona, tozi SlowComponent
+  //     syshto se rerendyrva. Zatova stoynostta na increase butona se uvelichava bavno (okolo polovin-edna
+  //     sekunda). Zaradi tova mozhem da izpolzvame optimization technique. */}
+  //     <SlowComponent />
+  //   </div>
+  // );
+
+  return (
+    <div>
+      <h1>Slow counter?!?</h1>
+      <Counter>
+        <SlowComponent />
+      </Counter>
     </div>
   );
 }
